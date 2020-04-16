266729966a4ae3824a229f39d71d713206c0fad7
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-15 23:17:19 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle``;
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
