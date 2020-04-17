c2abf65d54392e9d608bc7dc5976f764d7807b43
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-16 23:08:52 -0500

    Updates

---
+++ precache-manifest.cbec9ec22372e566a8c8fa2161e5edf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252238e11d68d1f7866d20ad41473e4d",
+    "url": "/react-bookstore-app/index.html"
+  },
+  {
+    "revision": "ac45e0148cb939b9da0e",
+    "url": "/react-bookstore-app/static/css/main.26f253f2.chunk.css"
+  },
+  {
+    "revision": "4287ff265ff1198ae3ac",
+    "url": "/react-bookstore-app/static/js/2.9d938063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bookstore-app/static/js/2.9d938063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac45e0148cb939b9da0e",
+    "url": "/react-bookstore-app/static/js/main.df63c322.chunk.js"
+  },
+  {
+    "revision": "90224eeb72a280de5089",
+    "url": "/react-bookstore-app/static/js/runtime-main.e623835e.js"
+  }
+]);
